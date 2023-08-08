daa1154fa255f0464339be98e0ba726e9a5c9692
Karan Suthar <kan.suthar666@gmail.com>
2023-08-08 23:36:29 +0530

    added grid layout ,added title widget and tab layout

---
+++ test.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+def on_entry_click(event):
+    if entry.get() == "Enter your text here":
+        entry.delete(0, "end")  # Clear the placeholder text when clicked
+        entry.config(fg="black")  # Change text color to black
+
+def on_focus_out(event):
+    if entry.get() == "":
+        entry.insert(0, "Enter your text here")
+        entry.config(fg="gray")  # Change text color back to gray
+
+root = tk.Tk()
+root.geometry("300x100")
+
+entry = tk.Entry(root, fg="gray")
+entry.insert(0, "Enter your text here")
+entry.bind("<FocusIn>", on_entry_click)
+entry.bind("<FocusOut>", on_focus_out)
+entry.pack()
+
+root.mainloop()
