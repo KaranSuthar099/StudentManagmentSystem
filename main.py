4ee416412a2b71cbfd5c05bdae6a00679e3cb717
Karan Suthar <kan.suthar666@gmail.com>
2023-08-12 10:43:12 +0530

    removed search bar due to layout issue

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ window = c.CTk()
 window.geometry("1280x720")
 window.title("School Management System")
 
-window.columnconfigure([0, 1, 2, 3, 4, 5, 6], weight=1)
-window.rowconfigure([0, 1, 2, 3, 4, 5, 6], weight=1)
+window.columnconfigure([0, 1, 2, 3, 4], weight=1, uniform="uniform columns")
+window.rowconfigure([0, 1, 2, 3, 4, 5, 6], weight=1, uniform="uniform rows")
 # window.rowconfigure(1, weight=9)
 
 sidebar = c.CTkFrame(window, corner_radius=20, width=120)
@@ -23,7 +23,6 @@ tabs = c.CTkTabview(window, corner_radius=20)
 tabs.add("Insert Student Data")
 tabs.add("Update Student Data")
 tabs.add("Delete Student Data")
-tabs.add("Search Student Data")
 
 RollNo = c.CTkLabel(tabs.tab("Insert Student Data"), text="RollNo")  # primary key
 Name = c.CTkLabel(tabs.tab("Insert Student Data"), text="Name")
@@ -34,14 +33,14 @@ PhoneNumber = c.CTkLabel(tabs.tab("Insert Student Data"), text="Phone")
 GuardianName = c.CTkLabel(tabs.tab("Insert Student Data"), text="GuardianName")
 Address = c.CTkLabel(tabs.tab("Insert Student Data"), text="Address")
 
-RollNo.grid(row=1, column=1)
-Name.grid(row=2, column=1)
-Class.grid(row=3, column=1)
-DateOfBirth.grid(row=4, column=1)
-Gender.grid(row=5, column=1)
-PhoneNumber.grid(row=6, column=1)
-GuardianName.grid(row=7, column=1)
-Address.grid(row=8, column=1)
+RollNo.grid(row=0, column=1)
+Name.grid(row=1, column=1)
+Class.grid(row=2, column=1)
+DateOfBirth.grid(row=3, column=1)
+Gender.grid(row=4, column=1)
+PhoneNumber.grid(row=5, column=1)
+GuardianName.grid(row=6, column=1)
+Address.grid(row=7, column=1)
 
 EntryRollNumber = c.CTkEntry(tabs.tab("Insert Student Data"), placeholder_text="Enter Roll Number")
 
@@ -64,28 +63,24 @@ EntryGuardianName = c.CTkEntry(tabs.tab("Insert Student Data"), placeholder_text
 
 EntryAddress = c.CTkTextbox(tabs.tab("Insert Student Data"), width=30)
 
-EntryRollNumber.grid(row=1, column=2, padx=5, pady=5)
+EntryRollNumber.grid(row=0, column=2, padx=5, pady=5)
 
-EntryFirstName.grid(row=2, column=2, padx=5, pady=5)
-EntrySecondName.grid(row=2, column=3, padx=5, pady=5)
+EntryFirstName.grid(row=1, column=2, padx=5, pady=5)
+EntrySecondName.grid(row=1, column=3, padx=5, pady=5)
 
-EntryClass.grid(row=3, column=2, padx=5, pady=5)
+EntryClass.grid(row=2, column=2, padx=5, pady=5)
 
-EntryDay.grid(row=4, column=2, padx=5, pady=5)
-EntryMonth.grid(row=4, column=3, padx=5, pady=5)
-EntryYear.grid(row=4, column=4, padx=5, pady=5)
+EntryDay.grid(row=3, column=2, padx=5, pady=5)
+EntryMonth.grid(row=3, column=3, padx=5, pady=5)
+EntryYear.grid(row=3, column=4, padx=5, pady=5)
 
-EntryMaleGender.grid(row=5, column=2, padx=5, pady=5)
-EntryFemaleGender.grid(row=5, column=3, padx=5, pady=5)
+EntryMaleGender.grid(row=4, column=2, padx=5, pady=5)
+EntryFemaleGender.grid(row=4, column=3, padx=5, pady=5)
 
-EntryPhoneNumber.grid(row=6, column=2, padx=5, pady=5, columnspan=2, sticky="ew")
-EntryGuardianName.grid(row=7, column=2, padx=5, pady=5, columnspan=2, sticky="ew")
-EntryAddress.grid(row=8, column=2, padx=5, pady=5, columnspan=2, sticky="news")
+EntryPhoneNumber.grid(row=5, column=2, padx=5, pady=5, columnspan=2, sticky="ew")
+EntryGuardianName.grid(row=6, column=2, padx=5, pady=5, columnspan=2, sticky="ew")
+EntryAddress.grid(row=7, column=2, padx=5, pady=5, columnspan=2, sticky="news")
 
-tabs.grid(row=0, column=1, columnspan=4, rowspan=7, sticky="news", padx=10, pady=10)
-
-SearchBar = c.CTkEntry(window, placeholder_text="Search")
-SearchBar.grid(row=0, column= 5, sticky="ew")
-
+tabs.grid(row=0, column=1, columnspan=2, rowspan=6, sticky="news", padx=10, pady=10)
 
 window.mainloop()
