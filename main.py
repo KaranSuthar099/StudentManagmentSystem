1fd1479f3e80a792c5fb13d9e33f70fd4a44cfdc
Karan Suthar <kan.suthar666@gmail.com>
2023-09-25 22:21:48 +0530

    remaining SQL implementations and data insert into student table function added --

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import tkinter as tk
 import mysql.connector as sql
 import customtkinter as c
 
+
 c.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 c.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
+
 flagFirstRun = 0
 value_check = 0
-var_row=0
+var_row = 0
+
 
 def remove_focus_sidebar(_):
     # Remove focus from the widget that currently has focus
@@ -20,6 +23,25 @@ def remove_focus_tabs(_):
     Tabs.focus_set()
 
 current_row = 4
+
+def insert_data_student_record(roll, f_name, s_name, class_number, date, gender, phone_number, guardian_name, address):
+    name = f_name + s_name
+
+    if gender == 0:
+        gender = "Male"
+    else:
+        gender = "Female"
+
+    query = "INSERT INTO STUDENT_RECORDS (roll_number, name, class, Date_Of_Birth, Gender, Phone_Number, Guardians_Name, Address)" \
+            " VALUES ({}, '{}', {}, '{}', '{}', '{}', '{}', '{}')".format(
+        str(roll), name, str(class_number), date, gender, str(phone_number), guardian_name, address)
+    try:
+        cursor.execute(query)
+        mydb.commit()
+        print("data entry success")
+    except Exception as e:
+        print("ERROR!!! ", e)
+
 
 def create_subject():
     global current_row
@@ -90,7 +112,7 @@ def create_update_window_widgets(tab):
 
 
 def create_insert_window_widgets(tab):
-    roll_no_label = c.CTkLabel(Tabs.tab(tab), text="RollNo")  # primary key
+    roll_no_label = c.CTkLabel(Tabs.tab(tab), text="Roll NUmber")  # primary key
     roll_no_label.grid(row=0, column=1)
 
     name_label = c.CTkLabel(Tabs.tab(tab), text="Name")
@@ -99,7 +121,7 @@ def create_insert_window_widgets(tab):
     class_label = c.CTkLabel(Tabs.tab(tab), text="Class")
     class_label.grid(row=2, column=1)
 
-    date_of_birth_label = c.CTkLabel(Tabs.tab(tab), text="DateOfBirth")
+    date_of_birth_label = c.CTkLabel(Tabs.tab(tab), text="Date Of Birth")
     date_of_birth_label.grid(row=3, column=1)
 
     gender_label = c.CTkLabel(Tabs.tab(tab), text="Gender")
@@ -108,13 +130,13 @@ def create_insert_window_widgets(tab):
     phone_number_label = c.CTkLabel(Tabs.tab(tab), text="Phone")
     phone_number_label.grid(row=5, column=1)
 
-    guardian_name_label = c.CTkLabel(Tabs.tab(tab), text="GuardianName")
+    guardian_name_label = c.CTkLabel(Tabs.tab(tab), text="Guardian's Name")
     guardian_name_label.grid(row=6, column=1)
 
     address_label = c.CTkLabel(Tabs.tab(tab), text="Address")
     address_label.grid(row=7, column=1)
 
-    entry_roll_number = c.CTkEntry(Tabs.tab(tab), placeholder_text="Enter Roll Number")
+    entry_roll_number = c.CTkEntry(Tabs.tab(tab), placeholder_text="roll number")
     entry_roll_number.grid(row=0, column=2, padx=5, pady=5)
 
     entry_first_name = c.CTkEntry(Tabs.tab(tab), placeholder_text="First Name")
@@ -122,20 +144,16 @@ def create_insert_window_widgets(tab):
     entry_second_name = c.CTkEntry(Tabs.tab(tab), placeholder_text="Last Name")
     entry_second_name.grid(row=1, column=3, padx=5, pady=5)
 
-    entry_class = c.CTkEntry(Tabs.tab(tab), placeholder_text="Enter Class")
+    entry_class = c.CTkEntry(Tabs.tab(tab), placeholder_text="Class")
     entry_class.grid(row=2, column=2, padx=5, pady=5)
 
-    entry_day = c.CTkEntry(Tabs.tab(tab), placeholder_text="Day")
-    entry_day.grid(row=3, column=2, padx=5, pady=5)
-    entry_month = c.CTkEntry(Tabs.tab(tab), placeholder_text="Month")
-    entry_month.grid(row=3, column=3, padx=5, pady=5)
-    entry_year = c.CTkEntry(Tabs.tab(tab), placeholder_text="Year")
-    entry_year.grid(row=3, column=4, padx=5, pady=5)
+    entry_date = c.CTkEntry(Tabs.tab(tab), placeholder_text="YYYY-MM-DD")
+    entry_date.grid(row=3, column=2, padx=5, pady=5)
 
-    radio_val = tk.IntVar(value=0)
-    entry_male_gender = c.CTkRadioButton(Tabs.tab(tab), text="Male", variable=radio_val, value=0)
+    gender_value = tk.IntVar(value=0)
+    entry_male_gender = c.CTkRadioButton(Tabs.tab(tab), text="Male", variable=gender_value, value=0)
     entry_male_gender.grid(row=4, column=2, padx=5, pady=5)
-    entry_female_gender = c.CTkRadioButton(Tabs.tab(tab), text="Female", variable=radio_val, value=1)
+    entry_female_gender = c.CTkRadioButton(Tabs.tab(tab), text="Female", variable=gender_value, value=1)
     entry_female_gender.grid(row=4, column=3, padx=5, pady=5)
 
     entry_phone_number = c.CTkEntry(Tabs.tab(tab), placeholder_text="Enter Phone number")
@@ -147,7 +165,15 @@ def create_insert_window_widgets(tab):
     entry_address = c.CTkTextbox(Tabs.tab(tab), width=30)
     entry_address.grid(row=7, column=2, padx=5, pady=5, columnspan=2, sticky="news")
 
-    enter_button = c.CTkButton(Tabs.tab(tab), text="Enter")
+    enter_button = c.CTkButton(Tabs.tab(tab), text="Enter",
+                               command=lambda: insert_data_student_record(
+                                   entry_roll_number.get(), entry_first_name.get(),
+                                   entry_second_name.get(), entry_class.get(),
+                                   entry_date.get(),
+                                   gender_value.get(), entry_phone_number.get(),
+                                   entry_guardian_name.get(), entry_address.get(1.0, c.END)
+                               )
+                               )
     enter_button.grid(row=8, column=2, padx=5, pady=30, columnspan=2, sticky="news")
 
 
@@ -167,7 +193,7 @@ def SelectUpdateValue(choice):
         dynamic_update_widget = c.CTkTextbox(Tabs.tab("Update Student Data"), width=30)
         dynamic_update_widget.grid(row=2, column=1, sticky="news", padx=5, pady=5, columnspan=2)
 
-
+# result table code in HERE
 def SelectTableMainFrame(choice):
     global Tabs
     Tabs.destroy()
@@ -240,7 +266,21 @@ mydb = sql.connect(host="localhost", user="root", passwd="root")  # Establishing
 
 cursor = mydb.cursor()  # Creating Cursor object
 
-cursor.execute("")
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS STUDENT_MANAGEMENT_SYSTEM")
+cursor.execute("USE STUDENT_MANAGEMENT_SYSTEM")
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS STUDENT_RECORDS (
+    Roll_Number INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Class INT,
+    Date_Of_Birth DATE,
+    Gender ENUM('Male', 'Female'),
+    Phone_Number BIGINT,
+    Guardians_Name VARCHAR(50),
+    Address VARCHAR(255)
+);
+''')
 
 # SQL implementations
 
@@ -248,8 +288,8 @@ cursor.execute("")
 
 window = c.CTk()
 window.geometry("1280x720")
-
 window.title("School Management System")
+
 window.columnconfigure(0, weight=1, uniform="uniform columns")
 window.columnconfigure(1, weight=1, uniform="uniform columns")
 window.columnconfigure(2, weight=1, uniform="uniform columns")
